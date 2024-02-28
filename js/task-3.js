c9ea16fa608365ef0d7683fc16a7d850c91c676e
stayprimitive <119599068+stayprimitive@users.noreply.github.com>
2024-02-29 00:30:44 +0200

    Task-3 - DONE!

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,21 @@
-function isEnoughCapacity(products, containerSize) {
-    let sum = 0;
-    let productsValues = Object.values(products);
-    for (const productValue of productsValues) {
-        sum += productValue;
-    }
-    if (sum <= containerSize) {
-        return true;
-    } else {
-        return false;
-    }
-    }
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
+    },
+};
 
-    console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
